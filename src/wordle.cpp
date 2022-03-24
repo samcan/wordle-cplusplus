13eedd779ca85c6628776c3a955edd89a992df2c
Sam Cantrell <samuel@samuelcantrell.com>
2022-03-23 18:34:18 -0700

    Force player to enter a 5-letter word before checking it

--- src/wordle.cpp
+++ src/wordle.cpp
@@ -7,6 +7,9 @@
 #include "wordlist.h"
 #include "puzzle.h"
 
+const int max_num_of_guesses = 6;
+const int wordlength = 5;
+
 void load_words(wordlist *list);
 
 int main() {
@@ -19,12 +22,17 @@ int main() {
 
     // get guesses
     std::string guess;
-    for (int i=0; i<6; i++) {
-        std::cout << "> ";
-        std::cin >> guess;
+    for (int i=0; i<max_num_of_guesses; i++) {
+        bool valid_guess_entered = false;
+        while (!valid_guess_entered) {
+            std::cout << "> ";
+            std::cin >> guess;
 
-        if (guess.length() > 5) {
-            guess = guess.substr(0, 5);
+            if (guess.length() != wordlength) {
+                std::cout << "Enter a valid 5-letter word." << std::endl;
+            } else {
+                valid_guess_entered = !valid_guess_entered;
+            }
         }
 
         // check guess
@@ -50,8 +58,8 @@ void load_words(wordlist *list) {
     f.open(filename.c_str());
 
     // each word is only 5 chars long plus the \n char
-    char word[6];
-    while (f.getline(word, 6)) {
+    char word[wordlength+1];
+    while (f.getline(word, wordlength+1)) {
         list->add(word);
     }
 
