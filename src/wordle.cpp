6687cc945f9e6c77c24977bc0bce1db3bde42706
Sam Cantrell <samuel@samuelcantrell.com>
2022-03-24 17:24:23 -0700

    Make the keyboard object a pointer

--- src/wordle.cpp
+++ src/wordle.cpp
@@ -14,9 +14,10 @@
 
 
 void load_words(wordlist *list);
-void update_keyboard(std::vector<key> result, keyboard &kbd);
+void update_keyboard(std::vector<key> result, keyboard *kbd);
 void prompt_for_guess(std::ostream& os, int current_guess, int number_of_guesses);
 void output_guess_result(std::ostream& os, std::string guess, std::vector<key> result);
+bool is_result_winning(std::vector<key> result);
 
 int main() {
     // load words into wordlist
@@ -28,16 +29,19 @@ int main() {
     std::cout << "done" << std::endl;
 
     // build keyboard
-    keyboard kbd;
+    keyboard* kbd = new keyboard();
 
     // create puzzle
     puzzle* puz = new puzzle(puzzle_answers->pop_last_word());
 
     // get guesses
+    bool quitting = false;
+    bool winning = false;
+    int i;
     std::string guess;
-    for (int i=0; i<max_num_of_guesses; i++) {
+    for (i=0; i<max_num_of_guesses; i++) {
         // display keyboard with keys remaining
-        std::cout << kbd << std::endl;
+        std::cout << *kbd << std::endl;
 
         bool valid_guess_entered = false;
         while (!valid_guess_entered) {
@@ -46,6 +50,11 @@ int main() {
             
             // transform guess to lowercase
             strtolower(guess);
+
+            if (guess == "quit") {
+                quitting = true;
+                break;
+            }
 
             if (guess.length() != wordlength) {
                 std::cout << "Enter a valid 5-letter word." << std::endl;
@@ -59,23 +68,50 @@ int main() {
             }
         }
 
+        if (quitting) {
+            break;
+        }
+
         // check guess
         std::vector<key> result = puz->check_answer(guess);
         
         // display guess and its result
         output_guess_result(std::cout, guess, result);
+        winning = is_result_winning(result);
         
         // update keyboard
         update_keyboard(result, kbd);
 
+        if (winning) {
+            break;
+        }
+
     }
+
+    if (!winning) {
+        // lost the game
+        std::cout << std::endl << "Sorry, but you lost! You ran out of guesses." << std::endl;
+    } else {
+        std::cout << std::endl << "Congratulations; you won in " << i+1 << " guesses." << std::endl;
+    }
+    // TODO display guess results
 
     // free up memory before quitting
     delete(puz);
     delete(puzzle_answers);
     delete(valid_guesses);
+    delete(kbd);
 
     return 0;
+}
+
+bool is_result_winning(std::vector<key> result) {
+    for (auto k : result) {
+        if (k.status != guess_status::Correct) {
+            return false;
+        }
+    }
+    return true;
 }
 
 void prompt_for_guess(std::ostream& os, int current_guess, int number_of_guesses) {
@@ -90,10 +126,10 @@ void output_guess_result(std::ostream& os, std::string guess, std::vector<key> r
     os << std::endl;
 }
 
-void update_keyboard(std::vector<key> result, keyboard& kbd) {
+void update_keyboard(std::vector<key> result, keyboard *kbd) {
     for (int i=0; i<wordlength; i++) {
         int j = static_cast<int>(result[i].name) - 'a';
-        kbd.keys[j].status = result[i].status;
+        kbd->keys[j].status = result[i].status;
     }
 }
 
